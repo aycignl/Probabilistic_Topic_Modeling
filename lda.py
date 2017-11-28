d2d3ce325dd34d027f3b73101e1eab4a7696cedd
gnl <aycignl@gmail.com>
2017-11-28 15:29:36 +0300

    Latent Dirichlet Allocation algorithm implementation

---
+++ lda.py
@@ -0,0 +1,81 @@
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+# create English stop words list
+en_stop = get_stop_words('en')
+
+# Create p_stemmer of class PorterStemmer
+p_stemmer = PorterStemmer()
+    
+# create sample documents
+doc_a = "Brocolli is good to eat. My brother likes to eat good brocolli, but not my mother."
+doc_b = "My mother spends a lot of time driving my brother around to baseball practice."
+doc_c = "Some health experts suggest that driving may cause increased tension and blood pressure."
+doc_d = "I often feel pressure to perform well at school, but my mother never seems to drive my brother to do better."
+doc_e = "Health professionals say that brocolli is good for your health." 
+doc_test = "Pickle is the standard way of serializing objects in Python."
+
+# compile sample documents into a list
+doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
+doc_set2 = [doc_test]
+# list for tokenized documents in loop
+texts = []
+
+# loop through document list
+for i in doc_set:
+    
+    # clean and tokenize document string
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in en_stop]
+    
+    # stem tokens
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    
+    # add tokens to list
+    texts.append(stemmed_tokens)
+
+# turn our tokenized documents into a id <-> term dictionary
+dictionary = corpora.Dictionary(texts)
+    
+# convert tokenized documents into a document-term matrix
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+texts2 = []
+# loop through document list
+for i in doc_set2:
+    
+    # clean and tokenize document string
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in en_stop]
+    
+    # stem tokens
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    
+    # add tokens to list
+    texts2.append(stemmed_tokens)
+
+# turn our tokenized documents into a id <-> term dictionary
+dictionary2 = corpora.Dictionary(texts2)
+    
+# convert tokenized documents into a document-term matrix
+corpus2 = [dictionary2.doc2bow(text) for text in texts2]
+
+# generate LDA model
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=3, id2word = dictionary, passes=20)
+print(ldamodel.print_topics(num_topics=3, num_words=4))
+
+doc_lda = ldamodel[corpus2]
+print
+print(doc_lda.print_topics(5))
+
