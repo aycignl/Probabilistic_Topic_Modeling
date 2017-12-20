653f204791d6f733c463b0536c1354e4f1620e00
Dilara Keküllüoğlu <dilarahae@gmail.com>
2017-12-20 14:33:37 +0200

    LDA training with given file and the corresponding probabilities are calculated.

--- train_lda.py
+++ train_lda.py
@@ -1,6 +1,3 @@
-from nltk.tokenize import RegexpTokenizer
-from stop_words import get_stop_words
-from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 import gensim
 
@@ -13,13 +10,25 @@ def train_lda(train_filename):
 	train_data = train_file.readlines()
 	train_kmers = []
 	for prot in train_data:
-		info = prot.split(",")
+		print(prot)
+		info = prot.split()
 		kmers = info[2:]
-		train_kmers.append(kmer)
+		train_kmers.append(kmers)
 
+	print(len(train_kmers))
+	print(train_kmers[2])
 	dictionary = corpora.Dictionary(train_kmers)
+
 	corpus = [dictionary.doc2bow(text) for text in train_kmers]
 
-	ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10000, id2word = dictionary, passes=20) #topic number selection???
+	print(len(corpus))
+	ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary, passes=20) #topic number selection??? according to family?
 
+	result_dict = {}
+	result_dict['ldamodel'] = ldamodel
 
+	doc_lda = ldamodel[corpus]
+
+	result_dict['train_topics'] = doc_lda
+
+	return result_dict
